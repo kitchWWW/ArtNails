6fcc1d6a9052769d72948e09c62faaca23d47956
kitchWWW <brian.e2014@gmail.com>
2024-04-01 22:53:27 -0400

    idk

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // App.js
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
